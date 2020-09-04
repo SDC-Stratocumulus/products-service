de4cf01e03c6b16f530c3211a37a304d1f1b77ac
Nuri Kim <nuri.g.kim@gmail.com>
2020-09-03 23:11:51 -0400

    Remove pg admin

--- database/index.js
+++ database/index.js
@@ -2,8 +2,6 @@ const {Pool} = require("pg");
 const config = require("./config.js");
 const pool = new Pool(config);
 
-//const db = pool.connect();
-
 pool.on("error", (err, client) => {
   console.log("Unexpected error on idle client", err);
 });
--- database/schema.sql
+++ database/schema.sql
@@ -6,9 +6,9 @@ CREATE DATABASE sdc;
 CREATE TABLE products
 (
   id SERIAL PRIMARY KEY,
-  product_name VARCHAR(300) DEFAULT null,
+  name VARCHAR(300) DEFAULT null,
   slogan VARCHAR(500) DEFAULT null,
-  product_descr VARCHAR(1000) DEFAULT null,
+  description VARCHAR(1000) DEFAULT null,
   category VARCHAR(100) DEFAULT null,
   default_price VARCHAR(20) DEFAULT null
 );
@@ -16,7 +16,7 @@ CREATE TABLE products
   CREATE TABLE related
   (
     id SERIAL PRIMARY KEY,
-    product_id INT(15) NOT NULL,
+    product_id INT NOT NULL,
     related_products_id INT(15) DEFAULT null
   );
 
@@ -25,32 +25,32 @@ CREATE TABLE products
   (
     id SERIAL PRIMARY KEY,
     product_id INT NOT NULL,
-    feature_name VARCHAR(500) DEFAULT null,
-    feature_value VARCHAR(500) DEFAULT null
+    feature VARCHAR(500) DEFAULT null,
+    value VARCHAR(500) DEFAULT null
   );
 
   CREATE TABLE styles
   (
     id SERIAL PRIMARY KEY,
     product_id INT NOT NULL,
-    style_name VARCHAR(300) DEFAULT null,
+    name VARCHAR(300) DEFAULT null,
     sale_price VARCHAR(20) DEFAULT null,
-    price VARCHAR(20) DEFAULT null,
+    original_price VARCHAR(20) DEFAULT null,
     default_style BOOLEAN DEFAULT false
   );
 
   CREATE TABLE photos
   (
     id SERIAL PRIMARY KEY,
-    style_id INT(15) NOT NULL,
-    full_size_url VARCHAR(1000) DEFAULT null,
-    thumbnail_url VARCHAR(1000) DEFAULT null
+    style_id INT NOT NULL,
+    url VARCHAR DEFAULT null,
+    thumbnail_url VARCHAR DEFAULT null
   );
 
   CREATE TABLE skus
   (
     id SERIAL PRIMARY KEY,
-    style_id INT(15) NOT NULL,
+    style_id INT NOT NULL,
     size VARCHAR(100) DEFAULT null,
     quantity VARCHAR(50) DEFAULT null
   );
